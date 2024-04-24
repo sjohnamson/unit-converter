f6f8857ba0f0e9ff33d24cb8f4a12b7350035c99
Sam Johnson <s_johnso@yahoo.com>
2024-04-24 16:48:32 -0500

    Add additional row feature in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,101 +8,160 @@ import { temperatures, volumes } from "./lib/units";
 import React, { useState } from "react";
 
 export default function Home() {
-  const [questionState, setQuestionState] = useState({
-    inputValue: 0,
-    startingUnit: 'Starting Unit',
-    endingUnit: 'Ending Unit',
-    studentAnswer: 0,
-  });
+  const [questionsState, setQuestionsState] = useState([
+    {
+      inputValue: 0,
+      startingUnit: "Starting Unit",
+      endingUnit: "Ending Unit",
+      studentAnswer: 0,
+      isValid: true,
+      isCorrect: false,
+      buttonClicked: false,
+    },
+  ]);
 
-  const [isValid, setIsValid] = useState(true);
-  const [isCorrect, setIsCorrect] = useState(false);
-  const [buttonClicked, setButtonClicked] = useState(false);
+  const handleInputChange = (
+    rowIndex,
+    name: string,
+    value: string | number
+  ) => {
+    setQuestionsState((prevState) => {
+      const updatedQuestions = [...prevState];
+      updatedQuestions[rowIndex][name] = value;
+      return updatedQuestions;
+    });
+  };
 
-  const handleInputChange = (name: string, value: string | number) => {
-    setQuestionState((prevState) => ({
+  const addRow = () => {
+    setQuestionsState((prevState) => [
       ...prevState,
-      [name]: value,
-    }));
+      {
+        inputValue: 0,
+        startingUnit: "Starting Unit",
+        endingUnit: "Ending Unit",
+        studentAnswer: 0,
+        isValid: true,
+        isCorrect: false,
+        buttonClicked: false,
+      },
+    ]);
   };
 
   const handleCheckButton = () => {
-    setButtonClicked(true);
-    const { inputValue, startingUnit, endingUnit, studentAnswer } =
-      questionState;
-      
-    if (
-      isNaN(inputValue) ||
-      isNaN(studentAnswer) ||
-      startingUnit === 'Starting Unit' ||
-      endingUnit === 'Ending Unit' 
-    ) {
-      setIsValid(false);
-      return;
-    }
+    setQuestionsState((prevState) =>
+      prevState.map((questionState) => {
+        const { inputValue, startingUnit, endingUnit, studentAnswer } =
+          questionState;
+        let isValid = true;
+        let isCorrect = false;
+        let buttonClicked = true;
 
-    let answer;
-    if (temperatures.includes(startingUnit)) {
-      answer = convertTemperature(questionState).toFixed(1);
-    } else {
-      answer = convertVolume(questionState).toFixed(1);
-    }
+        if (
+          isNaN(inputValue) ||
+          isNaN(studentAnswer) ||
+          startingUnit === "Starting Unit" ||
+          endingUnit === "Ending Unit"
+        ) {
+          isValid = false;
+          return { ...questionState, isValid, isCorrect, buttonClicked };
+        }
 
-    const isAnswerCorrect = answer === studentAnswer.toFixed(1);
-    setIsCorrect(isAnswerCorrect);
+        let answer;
+        if (temperatures.includes(startingUnit)) {
+          answer = convertTemperature(questionState).toFixed(1);
+        } else {
+          answer = convertVolume(questionState).toFixed(1);
+        }
+
+        isCorrect = answer === studentAnswer.toFixed(1);
+        return { ...questionState, isValid, isCorrect, buttonClicked };
+      })
+    );
   };
 
   return (
     <main className="flex flex-col min-h-screen items-center justify-center p-4 md:p-24">
-      <div className="mt-5 flex flex-col items-center gap-2 md:mt-8 md:flex-row md:gap-4">
-        {/* returns input fields for the basic question structure */}
-        <Inputs
-          label="Input Value"
-          name="inputValue"
-          placeholder="0"
-          value={questionState.inputValue}
-          onChange={handleInputChange}
-        />
-        <Dropdowns
-          label="Starting Unit"
-          name="startingUnit"
-          value={questionState.startingUnit}
-          onChange={handleInputChange}
-          options={["Starting Unit", ...temperatures, ...volumes]}
-        />
-        <Dropdowns
-          label="Ending Unit"
-          name="endingUnit"
-          value={questionState.endingUnit}
-          onChange={handleInputChange}
-          options={
-            questionState.startingUnit
-              ? temperatures.includes(questionState.startingUnit)
-                ? ["Ending Unit", ...temperatures]
-                : ["Ending Unit", ...volumes]
-              : ["Ending Unit"]
-          }
-        />
-        <Inputs
-          label="Student Answer"
-          name="studentAnswer"
-          placeholder="0"
-          value={questionState.studentAnswer}
-          onChange={handleInputChange}
-        />
-        {/* result is displayed after "check answers" button is clicked */}
-        <Results
-          isValid={isValid}
-          isCorrect={isCorrect}
-          buttonClicked={buttonClicked}
-        />
-      </div>
+      {questionsState.map(
+        (
+          {
+            isValid,
+            isCorrect,
+            buttonClicked,
+            inputValue,
+            startingUnit,
+            endingUnit,
+            studentAnswer,
+          },
+          rowIndex
+        ) => (
+          <div
+            key={rowIndex}
+            className="mt-5 flex flex-col items-center gap-2 md:mt-8 md:flex-row md:gap-4"
+          >
+            {/* returns input fields for the basic question structure */}
+            <Inputs
+              label="Input Value"
+              name="inputValue"
+              placeholder="0"
+              value={inputValue}
+              onChange={(name, value) =>
+                handleInputChange(rowIndex, name, value)
+              }
+            />
+            <Dropdowns
+              label="Starting Unit"
+              name="startingUnit"
+              value={startingUnit}
+              onChange={(name, value) =>
+                handleInputChange(rowIndex, name, value)
+              }
+              options={["Starting Unit", ...temperatures, ...volumes]}
+            />
+            <Dropdowns
+              label="Ending Unit"
+              name="endingUnit"
+              value={endingUnit}
+              onChange={(name, value) =>
+                handleInputChange(rowIndex, name, value)
+              }
+              options={
+                startingUnit
+                  ? temperatures.includes(startingUnit)
+                    ? ["Ending Unit", ...temperatures]
+                    : ["Ending Unit", ...volumes]
+                  : ["Ending Unit"]
+              }
+            />
+            <Inputs
+              label="Student Answer"
+              name="studentAnswer"
+              placeholder="0"
+              value={studentAnswer}
+              onChange={(name, value) =>
+                handleInputChange(rowIndex, name, value)
+              }
+            />
+            {/* result is displayed after "check answers" button is clicked */}
+            <Results
+              isValid={isValid}
+              isCorrect={isCorrect}
+              buttonClicked={buttonClicked}
+            />
+          </div>
+        )
+      )}
       <div className="mt-5 flex gap-2 md:mt-8 md:gap-4">
         <button
           className="flex h-10 items-right rounded-lg bg-blue-500 p-2  text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50"
           onClick={handleCheckButton}
         >
           Check Answers
+        </button>
+        <button
+          className="flex h-10 items-right rounded-lg bg-blue-500 p-2  text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50"
+          onClick={addRow}
+        >
+          Add Row
         </button>
       </div>
     </main>
