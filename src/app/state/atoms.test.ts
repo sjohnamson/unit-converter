e3b8a4c52519f050b3f9c2d74e1e82128fb71aae
samjohnson <171059116+samjohnsonflexion@users.noreply.github.com>
2024-08-16 12:23:13 -0500

    WIP - tests

--- src/app/state/atoms.test.ts
+++ src/app/state/atoms.test.ts
@@ -2,20 +2,28 @@
 import  '@testing-library/jest-dom';
 import { fireEvent, render } from '@testing-library/react';
 import { questionInputs, resultContentResponse } from './atoms';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 
 describe('recoil atoms state tests', () => {
     test(' return correct content and styling when button clicked if answer is correct', () => {
-       set questionInputs to:
-        buttonClicked: true,
-        isCorrect: true,
-        isValid: true,
-    
-    const actualRespnse = useRecoilValue(resultContentResponse);
+       const [_questionInputState, setQuestionInputState] = useRecoilState(questionInputs);
+       
+        setQuestionInputState([{
+            buttonClicked: true,
+            isCorrect: true,
+            isValid: true,
+            inputValue: 0,
+            startingUnit: '',
+            endingUnit: '',
+            correctAnswer: 0,
+            studentAnswer: 0
+        }])
+
+    const actualResponse = useRecoilValue(resultContentResponse);
     const expectedResponse = {content: {text: "Correct"}, styling: {border: "border-green-600", color: "text-green-600"} }
 
-    expect(actualRespnse).toEqual(expectedResponse);
+    expect(actualResponse).toEqual(expectedResponse);
 
     })
 })
